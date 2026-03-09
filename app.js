26e2fb3a17ce34dd1ae22a6def0c76a4502eb918
Kunjal Rajput <kunjalrajput06@gmail.com>
2026-03-10 00:31:30 +0530

    Project Created

--- app.js
+++ app.js
@@ -15,29 +15,28 @@ form.addEventListener("submit", async (evt) => {
     display.style.display = "block";
     const cityval = cityinput.value.trim();
     if (cityval === "") {
-   alert("Please enter a city name");
-   return;
+        alert("Please enter a city name");
+        return;
     }
 
     const finalURL = `${BASE_URL}?q=${cityval}&appid=a42640b4ecdeccefaf87c430901cfc32&units=metric`;
     
     city.innerText = "Loading...";
     
-
     let response;
 
     try {
-    response = await fetch(finalURL);
+        response = await fetch(finalURL);
     } catch (err) {
-    alert("Something went wrong");
-    return;
+        alert("Something went wrong");
+        return;
     }
 
     const data = await response.json();
 
     if (data.cod == 404) {
-    alert("City not found!");
-    return;
+        alert("City not found!");
+        return;
     }
 
     icon.style.display = "block";
@@ -46,19 +45,19 @@ form.addEventListener("submit", async (evt) => {
     const weatherMain = data.weather[0].main;
 
     if (weatherMain === "Clear") {
-    document.body.style.background = "linear-gradient(to right, #fceabb, #f8b500)";
+        document.body.style.background = "linear-gradient(to right, #fceabb, #f8b500)";
     }
     else if (weatherMain === "Clouds") {
-    document.body.style.background = "linear-gradient(to right, #bdc3c7, #2c3e50)";
+        document.body.style.background = "linear-gradient(to right, #bdc3c7, #2c3e50)";
     }
     else if (weatherMain === "Rain" || weatherMain === "Drizzle") {
-    document.body.style.background = "linear-gradient(to right, #4e54c8, #8f94fb)";
+        document.body.style.background = "linear-gradient(to right, #4e54c8, #8f94fb)";
     }
     else if (weatherMain === "Snow") {
-    document.body.style.background = "linear-gradient(to right, #e6dada, #274046)";
+        document.body.style.background = "linear-gradient(to right, #e6dada, #274046)";
     }
     else {
-    document.body.style.background = "linear-gradient(to right, #89f7fe, #66a6ff)";
+        document.body.style.background = "linear-gradient(to right, #89f7fe, #66a6ff)";
     }
 
     const iconCode = data.weather[0].icon;
@@ -73,7 +72,7 @@ form.addEventListener("submit", async (evt) => {
 
 locationBtn.addEventListener("click", () => {
     if (!navigator.geolocation) {
-    alert("Geolocation is not supported");
+        alert("Geolocation is not supported");
     }
     display.style.display = "block";
     navigator.geolocation.getCurrentPosition(async (position) => {
